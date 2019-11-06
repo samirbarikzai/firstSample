489ca0502a29dbe2806beb07092b887ed072b53b
mansoor224 <samirbarikzai@gmail.com>
2019-11-05 21:11:34 -0500

    hgcjj

---
+++ Java Classes/src/com/syntax/Sample.java
@@ -0,0 +1,14 @@
+package com.syntax;
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("This is my first program pushed to my remote repository");
+		
+		
+		
+	}
+
+}
